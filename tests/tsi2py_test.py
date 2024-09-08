d171405d8e51fdd79982b829fe5b88655821cf86
Danny Brown <dannybrown37@gmail.com>
2024-09-08 18:40:59 -0400

    remove unused const in test

--- tests/tsi2py_test.py
+++ tests/tsi2py_test.py
@@ -3,9 +3,6 @@ from pathlib import Path
 from tsi2py.parser import parse_interfaces
 
 
-INTERFACE_COUNT_IN_SAMPLE_FILE = 5
-
-
 def test_parser() -> None:
     path = str(Path(__file__).parent / 'interfaces' / 'sample.ts')
     interfaces = parse_interfaces(path)
