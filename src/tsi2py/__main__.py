b6d83295c93b3133d1af2d1cd3e334d98b5cbf42
Danny Brown <dannybrown37@gmail.com>
2024-09-08 17:51:50 -0400

    add basic click cli to take a file path, add a .gitignore

--- src/tsi2py/__main__.py
+++ src/tsi2py/__main__.py
@@ -1,99 +1,15 @@
-import re
 from pathlib import Path
 from pprint import pprint
 
+from tsi2py.parser import parse_interfaces
 
-def find_interfaces(text: str) -> list:
-    interface_pattern = re.compile(r'interface\s+(\w+)(<\w+>)?\s*{')
-    interfaces = []
-
-    for match in interface_pattern.finditer(text):
-        interface_name = match.group(1)
-        generic_type = match.group(2)  # Capture the generic type
-        start_index = match.end()
-        brace_count = 1
-        i = start_index
-
-        while brace_count > 0 and i < len(text):
-            if text[i] == '{':
-                brace_count += 1
-            elif text[i] == '}':
-                brace_count -= 1
-            i += 1
-
-        interface_body = text[start_index : i - 1].strip()
-        interfaces.append(
-            (
-                interface_name,
-                generic_type,
-                interface_body,
-            ),
-        )  # Include generic type
-
-    return interfaces
+import click
 
 
-def parse_interfaces(file_content: str) -> dict:
-    interfaces = {}
-    matches = find_interfaces(file_content)
-    for match in matches:
-        interface_name = match[0]
-        generic_type = match[1] if match[1] else None
-        interface_body = match[2]
-
-        attributes = {}
-        nested_object = {}
-        nested_object_key = None
-
-        for body_line in interface_body.splitlines():
-            line = body_line.strip()
-            if not line:
-                continue
-
-            # Detect and handle nested objects
-            if '{' in line:
-                nested_object_key = re.match(r'(\w+)\s*:\s*{', line)
-                if not nested_object_key:
-                    msg = f'Invalid line: {line}'
-                    raise ValueError(msg)
-                nested_object_key = nested_object_key.group(1)
-                nested_object = {}
-                attributes[nested_object_key] = nested_object
-                continue
-
-            if '}' in line:
-                attributes[nested_object_key] = nested_object
-                nested_object_key = None
-                continue
-
-            # Key-value pair within a nested object
-            if nested_object_key:
-                attr_match = re.match(r'(\w+)\s*:\s*([^;]+);', line)
-                if attr_match:
-                    attr_name = attr_match.group(1)
-                    attr_type = attr_match.group(2).strip()
-                    nested_object[attr_name] = attr_type
-                continue
-
-            # Normal key-value pair
-            attr_match = re.match(r'(\w+)\s*:\s*(.+?);', line)
-            if attr_match:
-                attr_name = attr_match.group(1)
-                attr_type = attr_match.group(2).strip()
-                attributes[attr_name] = attr_type
-
-        # If it's a generic interface, include the generic type in the key
-        if generic_type:
-            interface_name += generic_type
-
-        interfaces[interface_name] = attributes
-
-    return interfaces
-
-
-def main() -> None:
-    path = Path(__file__).parent / 'sample.ts'
-    with path.open('r') as f:
+@click.command()
+@click.argument('path', type=click.Path(exists=True, dir_okay=False))
+def main(path: click.Path) -> None:
+    with Path(str(path)).open('r') as f:
         data = f.read()
     interfaces = parse_interfaces(data)
     pprint(interfaces)
