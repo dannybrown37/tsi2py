5cf437f54ef46afa3e14e82d407a75f42132be89
Danny Brown <dannybrown37@gmail.com>
2024-09-09 18:22:36 -0400

    add a test for extension serialization

--- tests/extension_test.py
+++ tests/extension_test.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 from tsi2py.parser import parse_interfaces
+from tsi2py.serializer import serialize
 
 
 def test_parse_interfaces_with_extensions(data_path: Path) -> None:
@@ -28,3 +29,37 @@ def test_parse_interfaces_with_extensions(data_path: Path) -> None:
             'supervisorId': 'number',
         },
     }
+
+
+def test_serializer_with_extensions(data_path: Path) -> None:
+    path = str(data_path / 'extends.ts')
+    interfaces = parse_interfaces(path)
+    serialized = serialize(interfaces)
+    assert (
+        serialized
+        == """from typing import TypedDict, TypeVar, Literal, Generic, Any
+
+T = TypeVar('T')
+
+
+class Person(TypedDict):
+    id: int
+    name: str
+    age: int
+
+class Employee(TypedDict, Person):
+    employeeId: int
+    department: str
+
+class Manager(TypedDict, Employee):
+    teamSize: int
+    managesDepartments: list[str]
+
+class Supervisor(TypedDict, Person, Manager):
+    supervisorId: int
+
+class Response(TypedDict, Employee, Generic[T]):
+    data: T
+    status: str
+"""
+    )
